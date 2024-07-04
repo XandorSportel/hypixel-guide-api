7a9d58846169bf5d5f19207e780579239c183839
milanhaaijer <milanhaaijer2004@gmail.com>
2024-07-05 00:39:53 +0200

    fixed datarouter

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-const testDataRouter = require("./routes/guidedata");
+const guideDataRouter = require("./routes/guidedata");
 app.use(express.json());
 app.use(
     express.urlencoded({
